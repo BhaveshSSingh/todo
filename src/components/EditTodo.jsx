30ed138815823b1078da896e7f6913978fe474bd
Bhavesh S Singh <bhaveshsinghbiz@gmail.com>
2022-07-24 13:04:31 +0530

    refactor: Todo form feat: edit todo

--- src/components/EditTodo.jsx
+++ src/components/EditTodo.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
 
 function EditTodo() {
-  return <div>EditTodo</div>;
+  const [text, setText] = useState("");
+  const [day, setDay] = useState(new Date());
+  const [time, setTime] = useState(new Date());
+  function handleSubmit(e) {
+    console.log("Lmao");
+  }
+  const projects = [
+    {
+      id: 1,
+      name: "personal",
+      numOfTodos: 0,
+    },
+    { id: 2, name: "work", numOfTodos: 1 },
+    { id: 3, name: "other", numOfTodos: 2 },
+  ];
+  return (
+    <div className="EditTodo">
+      <div className="header">EditTodo</div>
+      <TodoForm
+        handleSubmit={handleSubmit}
+        text={text}
+        setText={setText}
+        day={day}
+        setDay={setDay}
+        time={time}
+        setTime={setTime}
+        projects={projects}
+      />
+    </div>
+  );
 }
 
 export default EditTodo;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  CheckCircleFill,
+  Circle,
+  ArrowClockwise,
+  Trash,
+} from "react-bootstrap-icons";
 
-function Todo() {
-  return <div>Todo</div>;
+function Todo({ todo }) {
+  const [hover, sethover] = useState(false);
+
+  return (
+    <div className="Todo">
+      <div
+        className="todo-container"
+        onMouseEnter={() => sethover(true)}
+        onMouseLeave={() => sethover(false)}
+      >
+        <div className="check-todo">
+          {todo.checked ? (
+            <span className="checked">
+              <CheckCircleFill color="#bebebe" />
+            </span>
+          ) : (
+            <span className="unchecked">
+              <Circle color={todo.color} />
+            </span>
+          )}
+        </div>
+        <div className="text">
+          <p style={{ color: todo.checked ? "#bebebe" : "#black" }}>
+            {todo.text}
+          </p>
+          <span>
+            {todo.time}-{todo.project}
+          </span>
+          <div className={`line ${todo.checked ? "line-through" : ""}`}></div>
+        </div>
+        <div className="add-to-next-day">
+          {todo.checked && (
+            <span>
+              <ArrowClockwise />
+            </span>
+          )}
+        </div>
+        <div className="delete-todo">
+          {(hover || todo.checked) && (
+            <span>
+              <Trash />
+            </span>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Todo;
